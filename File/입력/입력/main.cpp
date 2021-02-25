f39bbcb0fa40eea59b1ac9045f6d39a09eaaeaf4
daeho <dh.daeho@gmail.com>
2021-02-25 19:01:26 +0900

    C++ commit

---
+++ File/입력/입력/main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+void ReverseInputString();
+
+int main() {
+	int num;
+	int sum = 0;
+	while (true) {
+		cout << "enter an interger or EOF to quit : ";
+		cin >> num;
+		if (cin.eof()) {
+			break;
+		}
+		if (cin.fail()) {
+			cout << "Invalid input" << endl;
+			cin.clear();
+			cin.ignore(LLONG_MAX, '\n');
+			continue;
+		}
+		sum += num;
+	}
+	cin.clear();
+
+	cout << sum << endl;
+
+	cout << "-----------------------------" << endl;
+
+	ReverseInputString();
+}
+
+void ReverseInputString() {
+	const int LINE_SIZE = 512;
+	char line[LINE_SIZE];
+
+	cout << "enter an string to reverse" << endl
+		<< "	or EOF to quit : ";
+	cin.getline(line, LINE_SIZE);
+	if (cin.fail()) { // stringÀº eof fail
+		cin.clear();
+		return;
+	}
+	
+	char* p = line;
+	char* q = line + strlen(line) - 1;
+
+	while (p < q) {
+		char temp = *p;
+		*p = *q;
+		*q = temp;
+
+		++p;
+		--q;
+	}
+
+	cout << "Reverse string : " << line << endl;
+}
